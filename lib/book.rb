8dde85c7728abe7a0f4316c2963260c14b8cb9bd
ARoss1216 <a.ross24852@gmail.com>
2018-07-12 19:31:52 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,40 @@
 # book.rb
-# Add you Book class here
+class Book
+  def initialize(title) 
+    @title = title 
+  end
+  
+  def title
+    return @title 
+  end 
+  
+  def author=(author)
+    @author = author 
+  end
+  
+  def author
+    return @author
+  end 
+  
+  def page_count=(pagecount)
+    @page_count = pagecount
+  end
+  
+  def page_count
+    return @page_count
+  end 
+
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    return @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+  
+end
